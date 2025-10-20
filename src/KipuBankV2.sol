16d3e647e4acb25028a1429e5b9eaad1c6d95d32
HERNAN <hernan.ihf@gmail.com>
2025-10-20 00:31:43 -0300

    Create KipuBankV2 contract for ETH deposits

---
+++ src/KipuBankV2.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4 <0.9.0;
+
+/**
+ * @title KipuBank
+ * @author Hernán Iannello
+ * @notice Smart contract which allows users to deposit ETH
+ * in a personal vault and withdraw them with a limit per transaction.
+ */
+contract KipuBankV2 {
+}
